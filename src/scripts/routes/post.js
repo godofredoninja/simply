f2fddbb19dbd57f5fa6997fb03bd2ad61aa37655
GodoFredo <hell@godofredo.ninja>
2017-10-09 11:27:42 -0500

    fixed post action && add follow  social media in post for author

--- src/scripts/routes/post.js
+++ src/scripts/routes/post.js
@@ -2,6 +2,37 @@ import mediaEmbedResponsive from '../helper/video';
 import facebookShareCount from '../helper/facebook-share-count';
 import instagramFeed from '../helper/instagram-feed';
 
+// post action variables
+// const $postActions = $('.postActions');
+
+// let postActionScrollTimeOut = true;
+// let postActionLastPos = 0;
+// let postActionPos = 0;
+// let PostActionPosDelta = 5;
+
+// Function for show post action in post footer
+// function setPostActionsClass () {
+//   const PostBodyHeight = $('.post-body').outerHeight();
+
+//   postActionScrollTimeOut = false;
+//   postActionPos = $(window).scrollTop();
+
+//   if (postActionPos < PostBodyHeight) {
+//     if (Math.abs(postActionLastPos - postActionPos) >= PostActionPosDelta) {
+//       if (postActionPos > postActionLastPos && postActionPos > $postActions.outerHeight()) {
+//         $postActions.addClass('is-visible');
+//       } else {
+//         $postActions.removeClass('is-visible');
+//       }
+//       postActionLastPos = postActionPos;
+//     }
+//   } else {
+//     $postActions.removeClass('is-visible');
+//   }
+// }
+
+
+
 export default {
   init() {
     // Embed Video Post Format (Media Embed)
@@ -33,13 +64,12 @@ export default {
     if (typeof instagramUserId !== 'undefined' && typeof instagramToken !== 'undefined' && typeof instagramUserName !== 'undefined') {
       instagramFeed(instagramUserId, instagramToken, instagramUserName); // eslint-disable-line
     }
-    // show and hide post actions
-    setInterval( () => {
-      const scrollTop = $(window).scrollTop();
-      const heightPostBody = $('.post-body').height();
 
-      (scrollTop < heightPostBody) ? $('.postActions').addClass('is-visible') : $('.postActions').removeClass('is-visible');
-    }, 250);
+    // enable scroll time
+    // $(window).on('scroll', () => postActionScrollTimeOut = true);
+
+    // show and hide post actions
+    // setInterval( () => { if (postActionScrollTimeOut) setPostActionsClass() }, 250);
 
     /* Prism autoloader */
     Prism.plugins.autoloader.languages_path = `${$('body').attr('data-page')}/assets/scripts/prism-components/`; // eslint-disable-line
